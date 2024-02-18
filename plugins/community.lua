6cdf9023e49b8656df04a4f9a094b62ef72d3246
LIZUXUN <zuxun.li@quantil.com>
2024-02-18 10:09:00 +0800

    add package

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,14 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.programming-language-support.rest-nvim" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
